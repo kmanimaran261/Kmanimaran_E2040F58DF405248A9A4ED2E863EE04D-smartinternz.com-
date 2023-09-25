d03f627086a8454145afecee3ece00a8301e9ccb
kmanimaran261 <144419640+kmanimaran261@users.noreply.github.com>
2023-09-25 09:05:05 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-class BankAccount:
-    def __init__(self, account_number, account_holder_name, initial_balance=0.0):
-        self.__account_number = account_number
-        self.__account_holder_name = account_holder_name
-        self.__account_balance = initial_balance
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
-    def deposit(self, amount):
-        if amount > 0:
-            self.__account_balance += amount
-            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid deposit amount. Please enter a positive value.")
-
-    def withdraw(self, amount):
-        if amount > 0 and amount <= self.__account_balance:
-            self.__account_balance -= amount
-            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
-        else:
-            print("Invalid withdrawal amount or insufficient balance.")
-
-    def display_balance(self):
-        print(f"Account Holder: {self.__account_holder_name}")
-        print(f"Account Number: {self.__account_number}")
-        print(f"Account Balance: ${self.__account_balance}")
-
-
-# Create an instance of the BankAccount class
-account1 = BankAccount("123456789", "John Doe", 1000.0)
-
-# Test deposit and withdrawal functionality
-account1.display_balance()
-account1.deposit(500.0)
-account1.withdraw(200.0)
-account1.display_balance()
+num = int(input("Enter a number: "))
+result = factorial(num)
+print(f"The factorial of {num} is {result}")
